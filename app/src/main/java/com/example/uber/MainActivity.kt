04641afaffedcc58fe6670805f41d721900fb675
Nagaraj1027 <nagaraj.pambhala@gmail.com>
2021-05-05 08:35:16 +0530

    Explicit Intent

--- app/src/main/java/com/example/uber/MainActivity.kt
+++ app/src/main/java/com/example/uber/MainActivity.kt
@@ -9,25 +9,60 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.uber.implicitintent.ImplicitIntentExample
 import com.example.uber.intent.FirstActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     var btnGotoIntent: Button? = null
+    var btnGotoImplicitIntent: Button? = null
 
     //why ?/!!
+
+    //!!
+    //?
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         System.out.println("Hello")
 
+        initViews()
+
+        //Explicit Intent
+        //Implicit Intent
+    }
+
+    fun initViews() {
+
         btnGotoIntent = findViewById(R.id.btnGotoIntent) as Button
+        btnGotoImplicitIntent = findViewById(R.id.btnGotoImplicitIntent) as Button
 
-        btnGotoIntent!!.setOnClickListener(View.OnClickListener {
 
-            var gotoFirstAct = Intent(MainActivity@ this, FirstActivity::class.java)
-            startActivity(gotoFirstAct)
-        })
+        btnGotoIntent!!.setOnClickListener(this)
+        btnGotoImplicitIntent!!.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+
+        when (v!!.id) {
+
+            R.id.btnGotoIntent ->
+                gotoIntent()
+
+            R.id.btnGotoImplicitIntent ->
+                gotoImplicitIntent()
+        }
+
+    }
+
+    fun gotoIntent() {
+        var gotoFirstAct = Intent(MainActivity@ this, FirstActivity::class.java)
+        startActivity(gotoFirstAct)
+    }
+
+    fun gotoImplicitIntent() {
+        var gotoFirstAct = Intent(MainActivity@ this, ImplicitIntentExample::class.java)
+        startActivity(gotoFirstAct)
     }
 }
---
+++ app/src/main/java/com/example/uber/implicitintent/ImplicitIntentExample.kt
@@ -0,0 +1,36 @@
+package com.example.uber.implicitintent
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.uber.R
+
+class ImplicitIntentExample : AppCompatActivity() {
+
+    var btnImplicitIntent: Button? = null
+    var tvImplicitIntent: TextView? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_implicit)
+
+        Log.e("onCreate FirstAct ", "onCreate FirstAct ")
+
+        btnImplicitIntent = findViewById(R.id.btnImplicitIntent) as Button
+
+        btnImplicitIntent!!.setOnClickListener(View.OnClickListener {
+
+            var gotoImplicitIntent= Intent(Intent.ACTION_VIEW)
+            gotoImplicitIntent.data = Uri.parse("http://www.google.com")
+            startActivity(gotoImplicitIntent)
+        })
+
+
+    }
+}
